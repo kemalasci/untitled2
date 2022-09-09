003ff2cafd784b0c962607e2ed27384221afe9a7
Kemal Asci <kemalasci01.com>
2022-09-09 09:42:52 +0200

    java update

---
+++ src/test/java/dfgdfg.java
@@ -0,0 +1,22 @@
+public class dfgdfg {  public static void main(String[] args) {
+    System.out.println("södfgdfgkdfjg");
+}
+
+
+    public static void adsfasdf(){
+
+
+        System.out.println("dfsadefdsfsdfsdfsdf");
+
+
+    }
+
+
+
+    public void sdfsdfdsa(){
+
+        System.out.println("djfsdkjfdskf");
+
+
+    }
+}
